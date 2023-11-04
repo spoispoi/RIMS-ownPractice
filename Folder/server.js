0080a2c17d5fb79b2aad4b5c1f68cd4cdc40e700
Ian <ian1125757@gmail.com>
2023-11-04 17:39:42 +0800

    commit

--- Folder/server.js
+++ Folder/server.js
@@ -1,10 +1,21 @@
+const express = require('express')
+const app = express()
+
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./RIMS copy.db');
 
 
+// This line helps parse JSON body data
+app.use(express.json());
+
+const { readFile, readFileSync } = require('fs').promises;
+//const express = require('express');
+//const app = express();
+const PORT = 3000;
+
 //1. GET - Retrieve all items from the inventory table
 app.get('/inventory', (req, res) => {
-    const sql = "SELECT * FROM Inventory";
+    const sql = "SELECT * FROM Dish";
     db.all(sql, [], (err, rows) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -17,15 +28,15 @@ app.get('/inventory', (req, res) => {
 
   //2. POST - Insert a new item into the inventory table
   app.post('/inventory', (req, res) => {
-    const { ingredientID, quantityInStock, supplierOrderID, expirationDate } = req.body;
-    const sql = `INSERT INTO Inventory (IngredientID, QuantityInStock, SupplierOrderID, ExpirationDate) 
-                 VALUES (?, ?, ?, ?)`;
-    db.run(sql, [ingredientID, quantityInStock, supplierOrderID, expirationDate], function(err) {
+    const { InventoryID, IngredientID, QuantityInStock, SupplierOrderID, ExpirationDate } = req.body;
+    const sql = `INSERT INTO Inventory (InventoryID, IngredientID, QuantityInStock, SupplierOrderID, ExpirationDate) 
+                 VALUES (?, ?, ?, ?, ?)`;
+    db.run(sql, [InventoryID, IngredientID, QuantityInStock, SupplierOrderID, ExpirationDate], function(err) {
       if (err) {
         res.status(500).json({ error: err.message });
         return;
       }
-      res.json({ inventoryID: this.lastID });
+      res.json({ InventoryID: this.lastID });
     });
   });
 
@@ -33,12 +44,12 @@ app.get('/inventory', (req, res) => {
   //3. PUT - Update an item in the inventory table by ID
 
   app.put('/inventory/:id', (req, res) => {
-    const { id } = req.params;
-    const { ingredientID, quantityInStock, supplierOrderID, expirationDate } = req.body;
+    const {id} = req.params;
+    const { InventoryID, IngredientID, QuantityInStock, SupplierOrderID, ExpirationDate } = req.body;
     const sql = `UPDATE Inventory 
-                 SET IngredientID = ?, QuantityInStock = ?, SupplierOrderID = ?, ExpirationDate = ? 
+                 SET InventoryID = ?, IngredientID = ?, QuantityInStock = ?, SupplierOrderID = ?, ExpirationDate = ? 
                  WHERE InventoryID = ?`;
-    db.run(sql, [ingredientID, quantityInStock, supplierOrderID, expirationDate, id], function(err) {
+    db.run(sql, [InventoryID, IngredientID, QuantityInStock, SupplierOrderID, ExpirationDate], function(err) {
       if (err) {
         res.status(500).json({ error: err.message });
         return;
@@ -70,4 +81,8 @@ app.get('/inventory', (req, res) => {
     });
   });
   
-  
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
--- WEBAPP/server/server.js
+++ WEBAPP/server/server.js
@@ -5,4 +5,4 @@ app.get("/api", (req, res) => {
     res.json({ "users": ["userOne"]})
 })
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.listen(3000, () => {console.log("Server started on port 3000")})
