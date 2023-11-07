8f4f71060590e1c2a40d536a32b318ae6bed6b23
Ian <ian1125757@gmail.com>
2023-11-07 22:57:06 +0800

    commit

--- WEBAPP/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne"]})
-})
-
-app.listen(3000, () => {console.log("Server started on port 3000")})
